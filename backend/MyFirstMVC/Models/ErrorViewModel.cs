085da7d007f207e9a60050c7654985e06eadf87f
Osama Saleh <osamasaadeh170@gmail.com>
2025-09-21 00:08:16 +0300

    delete everything unnecessery

--- backend/MyFirstMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyFirstMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
